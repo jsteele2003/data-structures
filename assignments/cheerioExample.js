a0346244d3c803328629318a0055fe2b5600f565
Joe Steele <steej991@newschool.edu>
2017-09-11 16:41:08 +0000

    finished loop through address selectors

--- assignments/cheerioExample.js
+++ assignments/cheerioExample.js
@@ -7,23 +7,16 @@ var content = fs.readFileSync('data/page1.txt');
 // load `content` into a cheerio object
 var $ = cheerio.load(content);
 
-// print names of thesis students
-for( let j = 1; j < 5; j ++){
-    $('body > center > table > tbody > tr > td > table > tbody > tr:nth-child(2) > td > div > table > tbody > tr:nth-child(' + j + ') > td:nth-child(1)')
-    .each(callbackForEach(j));
-}
+// for( let j = 1; j < 23; j ++){
+//     //c & p selector from chrome tools
+//     $('body > center > table > tbody > tr > td > table > tbody > tr:nth-child(2) > td > div > table > tbody > tr:nth-child(' + j + ') > td:nth-child(1)')
+//     .each(callbackWrapperForEach(j));
+// }
 
-
-//named callback to create enclosing scope
-function callbackForEach(j){
-    return function(i, elem) {
-        console.log($(elem).text().trim());
-        console.log(j);
-    }
+for( let i = 1; i < 23; i ++){
+    //c & p selector from chrome tools
+    var elem = $('body > center > table > tbody > tr > td > table > tbody > tr:nth-child(2) > td > div > table > tbody > tr:nth-child(' + i + ') > td:nth-child(1)')
+    .text();
+    console.log(elem.split('\n')[3].trim());
     
 }
-
-// // print project titles
-// $('h').each(function(i, elem) {
-//     console.log($(elem).text());
-// });
